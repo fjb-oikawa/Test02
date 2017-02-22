c81ea54134e0445d8c868725d6d8861f3d727e01
Akihiko Oikawa <oikawa@chiba-fjb.ac.jp>
2017-02-22 11:39:26 +0900

    100回出力

--- src/Base.java
+++ src/Base.java
@@ -1,10 +1,9 @@
 
 public class Base {
-	final static int COUNT = 10;
+	final static int COUNT = 100;
 	final static String MSG = "Gitテスト";
 	public Base(){
 		for(int i=0;i<COUNT;i++)
 			System.out.println(MSG+i);
 	}
-
 }
